4fad16a9da10892d884417d483741525513b6551
marito02 <mariolavanderaa@gmail.com>
2022-10-27 19:51:00 +0200

    ej1

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print(f"el alumno {self.nombre} se ha creado correctamente")
+    def calificacion(self):
+        if self.nota >= 5:
+            print(f"el alumno {self.nombre} ha aprobado el examen con un {self.nota}")
+        else:
+            print(f"el alumno {self.nombre} ha suspendido el examen con un {self.nota}")
+
+alumno1=Alumno("Juan Gutierrez", 7)
+alumno2=Alumno("Javier Martinez", 4)
+alumno3=Alumno("Manolo Gomez", 8)
+
+lista = [alumno1, alumno2, alumno3]
+
+print(alumno1.calificacion())
+print(alumno2.calificacion())
+print(alumno3.calificacion())
---
+++ examen.parcialalgoritmos.md
@@ -0,0 +1,1 @@
+https://github.com/marito02/Examen_Parcial_27_Octubre_mario_lavandera.git
